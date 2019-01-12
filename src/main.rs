c3287f1f52fc53009c1d82843bc6b3070d570265
irieryota <ryotairie123@gmail.com>
2019-01-13 01:25:45 +0900

    とりあえずサンプルコピペでlambdaで動くようになった

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+
+use lambda_runtime::*;
+use serde_derive::*;
+
+use lambda_runtime::error::HandlerError;
+use std::error::Error;
+
+#[derive(Deserialize, Clone)]
+struct CustomEvent {
+    #[serde(rename = "firstName")]
+    first_name: String,
+}
+ 
+#[derive(Serialize, Clone)]
+struct CustomOutput {
+    message: String,
+}
+ 
+fn main() -> Result<(), Box<dyn Error>> {
+    lambda!(my_handler);
+    Ok(())
+}
+ 
+fn my_handler(e: CustomEvent, _c: lambda_runtime::Context) -> Result<CustomOutput, HandlerError> {
+    Ok(CustomOutput {
+        message: format!("Hello, {}!", e.first_name),
+    })
 }
